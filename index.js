effe58417513398b952e227213318ad24d5a20b9
João-BenedetMachado <105388117+joaobenedetmachado@users.noreply.github.com>
2022-08-17 14:50:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function Projeto1() {
+    window.location = "projetoum.html"
+}
+
+function Projeto2() {
+    window.location = "projetodois.html"
+}
+
+function Projeto3() {
+    window.location = "projetotreis.html"
+}
+
+function Projeto4() {
+    window.location = "projetoquatro.html"
+}
